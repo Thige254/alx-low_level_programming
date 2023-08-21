c6fc6538133b6fa4e5502f85aa01e543d5bd572f
Thige254 <thigetj@gmail.com>
2023-08-21 21:06:06 +0300

    .c files

---
+++ strcat.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#include "main.h"
+
+/**
+ * _strcat - Concatenate two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Pointer to the resulting string `dest`
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	return (dest);
+}
